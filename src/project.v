91fb74a24ab691266aa9ee1572c8da40bc2566b7
rojedas2000 <164827745+rojedas2000@users.noreply.github.com>
2026-01-07 21:27:48 -0600

    Update project.v
    segunda edicion, editar inputs y outputs, un input sera el rst

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_contador_display (
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-    assign uio_out [3:0] = 7'b0000;
+    assign uio_out [3:0] = 4'b0000;
   assign uio_oe  = 8'b1000_0000;
 
     wire [7:0] segmentos;
