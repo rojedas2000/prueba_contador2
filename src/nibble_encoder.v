63add962776d19e5be1b90dfaa04b9a5f3520d86
rojedas2000 <164827745+rojedas2000@users.noreply.github.com>
2026-01-08 17:16:23 -0600

    Update nibble_encoder.v

--- src/nibble_encoder.v
+++ src/nibble_encoder.v
@@ -13,16 +13,16 @@ always@(posedge clk)begin
 
 	case(nibble_in)
 	
-		4'd0:    disp_code = 8'b1_1000000; // 0
-      4'd1:    disp_code = 8'b1_1111001; // 1
-      4'd2:    disp_code = 8'b1_0100100; // 2
-      4'd3:    disp_code = 8'b1_0110000; // 3
-      4'd4:    disp_code = 8'b1_0011001; // 4
-      4'd5:    disp_code = 8'b1_0010010; // 5
-      4'd6:    disp_code = 8'b1_0000010; // 6
-      4'd7:    disp_code = 8'b1_1111000; // 7
-      4'd8:    disp_code = 8'b1_0000000; // 8
-      4'd9:    disp_code = 8'b1_0010000; // 9
+	  4'd0:    disp_code <= 8'b1_1000000; // 0
+      4'd1:    disp_code <= 8'b1_1111001; // 1
+      4'd2:    disp_code <= 8'b1_0100100; // 2
+      4'd3:    disp_code <= 8'b1_0110000; // 3
+      4'd4:    disp_code <= 8'b1_0011001; // 4
+      4'd5:    disp_code <= 8'b1_0010010; // 5
+      4'd6:    disp_code <= 8'b1_0000010; // 6
+      4'd7:    disp_code <= 8'b1_1111000; // 7
+      4'd8:    disp_code <= 8'b1_0000000; // 8
+      4'd9:    disp_code <= 8'b1_0010000; // 9
         default: disp_code <= 8'b1_1111111; // Apagado
 	
 	endcase
@@ -34,3 +34,4 @@ assign disp_code_out = disp_code;
 
 
 endmodule
+
