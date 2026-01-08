0f8df682ab68c2e8237977bebaa5d7ad88531e32
rojedas2000 <164827745+rojedas2000@users.noreply.github.com>
2026-01-07 20:11:26 -0600

    Update project.v
    1ra edicion

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_contador_display (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,25 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
+  // All output pins must be assigned. If not used, assign to 0.
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uio_out [3:0] = 7'b0000;
+  assign uio_oe  = 8'b1000_0000;
+
+    wire [7:0] segmentos;
+    wire [3:0] sel_seg;
+
+    assign uo_out [7:0] = segmentos;
+    assign uio_out [7:4] = sel_seg;
+    contador_display contador_display_Unit(
+
+        .clk(clk),
+        .rst(rst_n),
+        
+        
+    );
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, uio_in [7:0], ui_in [7:0], 1'b0};
 
 endmodule
